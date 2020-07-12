13bd7a006f221d15d879793869a3eff9fd91db0e
Preuel <68090703+Preuel@users.noreply.github.com>
2020-07-12 10:16:32 -0600

    Add files via upload

--- LineRunnerGame.java
+++ LineRunnerGame.java
@@ -14,14 +14,25 @@ public class LineRunnerGame extends JPanel implements ActionListener, KeyListene
     private JLabel walking2;
     private JLabel jump1;
     private JLabel jump2;
-    Timer timerWalk = new Timer(300,this);
-    Timer timerJump = new Timer(1, this);
-    int walkCounter = 0;
-    int jumpCounter = 0;
-    double jumpInitial = -0.7826;
-    double gravity = 0.001225;
-    int yinitial = 440;
-    int y = yinitial;
+    private JLabel duck;
+    private JLabel trashObstacle;
+    private Rectangle playerHitBox;
+    private Rectangle trashHitBox;
+    private Timer timerWalk = new Timer(5,this);
+    private Timer timerJump = new Timer(1, this);
+    private Timer timerObstacle = new Timer(5, this);
+    private int walkCounter = 0;
+    private int jumpCounter = 0;
+    private double backgroundVel = 2;
+    private double getBackgroundVelInitial = 2;
+    private int backgroundCounter = 0;
+    private double jumpInitial = -1;
+    private double gravity = 0.001875;
+    private int yinitial = 440;
+    private int y = yinitial;
+    public int score = 0;
+    private int randy = 600;
+    private int trashX = 1000;
 
     public LineRunnerGame(){
 
@@ -53,19 +64,56 @@ public class LineRunnerGame extends JPanel implements ActionListener, KeyListene
         Image jump2OrigImg = jump2Orig.getImage();
         Image jump2NewImg = jump2OrigImg.getScaledInstance(375,368, Image.SCALE_SMOOTH);
         jump2 = new JLabel(new ImageIcon(jump2NewImg));
-        jump2.setSize(375,368);
+        jump2.setSize(375,348);
         jump2.setLocation(35,y);
+        ImageIcon duckOrig = new ImageIcon("src/Duck.png");
+        Image duckOrigImg = duckOrig.getImage();
+        Image duckNewImg = duckOrigImg.getScaledInstance(435,240, Image.SCALE_SMOOTH);
+        duck = new JLabel(new ImageIcon(duckNewImg));
+        duck.setSize(435,240);
+        duck.setLocation(35,510);
+        ImageIcon trashOrig = new ImageIcon("src/trash.png");
+        Image trashOrigImg = trashOrig.getImage();
+        Image trashNewImg = trashOrigImg.getScaledInstance(135,165, Image.SCALE_SMOOTH);
+        trashObstacle = new JLabel(new ImageIcon(trashNewImg));
+        trashObstacle.setSize(135,165);
+        trashObstacle.setLocation(trashX,randy);
+        this.add(trashObstacle);
+        playerHitBox = new Rectangle(180, 430, 140, 330);
+
+
         timerWalk.start();
+        timerObstacle.start();
         this.setFocusable(true);
         this.requestFocusInWindow();
         this.setLayout(null);
         this.setVisible(true);
+
+
     }
 
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.BLACK);
         g.fillRect(0,70,LineRunner.screenWidth,5);
+        GradientPaint paint = new GradientPaint(0,0,new Color(54,81,214),0,LineRunner.screenHeight, new Color(114, 140, 155));
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(paint);
+        g2d.fillRect(0,75,LineRunner.screenWidth, LineRunner.screenHeight - 75);
+        g2d.setColor(new Color(18, 94, 34));
+        g2d.fillRect(0,550,LineRunner.screenWidth, LineRunner.screenHeight - 550);
+        g2d.setColor(new Color(112, 114, 116, 10));
+        g2d.fillRect(0,0,LineRunner.screenWidth, 70);
+        g2d.setColor(new Color(0,0,0));
+        g2d.setFont(new Font("Impact", Font.PLAIN, 50));
+        g2d.drawString("Line Runner", 5,53);
+        g2d.drawString("Score: ", 650, 53);
+        g2d.drawString(Integer.toString(score), 790, 53);
+//        g2d.drawString(Double.toString(backgroundVel), 50,100);
+//        g2d.drawRect(playerHitBox.x,playerHitBox.y,playerHitBox.width,playerHitBox.height);
+
+
+
 
 
     }
@@ -81,38 +129,79 @@ public class LineRunnerGame extends JPanel implements ActionListener, KeyListene
         // TODO Auto-generated method stub
         if (e.getKeyCode() == (KeyEvent.VK_UP)) {
             timerWalk.stop();
-
-
             timerJump.start();
-
-
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN && !timerJump.isRunning()){
+            timerWalk.stop();
+            this.remove(walking2);
+            this.remove(walking1);
+            this.add(duck);
+            playerHitBox.height = 220;
+            playerHitBox.y = 510;
+            this.revalidate();
+            this.repaint();
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
 
+        if (e.getKeyCode() == (KeyEvent.VK_DOWN) && !timerJump.isRunning()){
+            this.remove(duck);
+            playerHitBox.height = 330;
+            playerHitBox.y = 430;
+            timerWalk.start();
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object action = e.getSource();
         if (action.equals(timerWalk)){
-            this.remove(jump2);
-            this.remove(jump1);
-            if (walkCounter % 2 == 0){
+            if (walkCounter <= 60){
                 this.remove(walking1);
                 this.add(walking2);
                 this.revalidate();
                 this.repaint();
-            } else {
+            } else if (walkCounter <= 120){
                 this.remove(walking2);
                 this.add(walking1);
                 this.revalidate();
                 this.repaint();
+            } else {
+                walkCounter = 0;
             }
             walkCounter++;
+        } else if (action.equals(timerObstacle)) {
+            backgroundCounter ++;
+            trashX = trashX - (int)(backgroundVel);
+            trashHitBox = trashObstacle.getBounds();
+            backgroundVel = getBackgroundVelInitial + (Math.log(backgroundCounter)/100);
+            if (trashX < -135){
+                trashX = 1000 + (int) (Math.random()*1000);
+                // rand y from 550 - 635
+                randy = 555 + (int) (Math.random()*80);
+            }
+            trashObstacle.setLocation(trashX,randy);
+            Rectangle result = SwingUtilities.computeIntersection(
+                    (int)playerHitBox.getX(),
+                    (int)playerHitBox.getY(),
+                    (int) playerHitBox.getWidth(),
+                    (int)playerHitBox.getHeight(),
+                    trashHitBox);
+            if(result.getHeight() > 0 && result.getWidth() > 0){
+                timerWalk.stop();
+                timerObstacle.stop();
+                timerJump.stop();
+                LineRunner.gameContainer.remove(this);
+                LineRunner.gameContainer.add(new DeathScreen());
+                LineRunner.gameContainer.revalidate();
+                LineRunner.gameContainer.repaint();
+            }
+            score = (int) (backgroundCounter*backgroundVel/20);
+            this.revalidate();
+            this.repaint();
+
+
         } else if (action.equals(timerJump)){
             double delY = (jumpInitial*(jumpCounter)) + (0.5*gravity*jumpCounter*jumpCounter);
             double Velocity = jumpInitial + gravity*jumpCounter;
@@ -129,15 +218,24 @@ public class LineRunnerGame extends JPanel implements ActionListener, KeyListene
                 jump1.setLocation(30,y);
                 this.add(jump1);
             }
+            playerHitBox.y = 430 + change;
             this.revalidate();
             this.repaint();
 
             jumpCounter++;
             if (y > yinitial){
+                timerWalk.start();
                 timerJump.stop();
+                this.remove(jump2);
+                this.remove(jump1);
+                this.add(walking1);
+                playerHitBox.y = 430;
+                this.revalidate();
+                this.repaint();
+                walkCounter = 1;
                 y = yinitial;
                 jumpCounter = 0;
-                timerWalk.start();
+
 
             }
         }
